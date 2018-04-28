84dd60aca26fa8862a8b9051bc34d268e9ff86c8
lagron1000 <lagron1000@gmail.com>
2018-04-28 13:09:01 +0300

    EXTENSIONs 1-2

--- script.js
+++ script.js
@@ -5,11 +5,6 @@ var fetch = function () {
       url: 'https://www.googleapis.com/books/v1/volumes?q=intitle:'+ibsnNum,
       success: function(data) {
         $(".book").empty()
-          var bookTitle = data.items[0].volumeInfo.title;
-          var bookDescription = data.items[0].volumeInfo.description
-          var bookAuthor = data.items[0].volumeInfo.authors;
-          var bookImg = data.items[0].volumeInfo.imageLinks.thumbnail
-
           var source = $('#books-template').html();
           var template = Handlebars.compile(source);
           var newHTML = template(data);
@@ -25,6 +20,6 @@ $(".search-book").on('click', function(){
     fetch();
 })
 
-// $("li").on('click', function(){
-//   $(this).siblings().empty()
-// })
+$(".book").on('click', '.bookLi', function(){
+  $(this).siblings().remove()
+})
